fe747db289dca2bc9a2a224eae537b5613c9b4d9
kumarnutan204 <74036964+kumarnutan204@users.noreply.github.com>
2023-03-11 18:20:23 +0530

    Rohan is planning to paint his house

---
+++ oops/Rohan is planning.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void l()
+{
+    printf("class ReceiveMesurement class CalculateArea : public ReceiveMesurement CalculateArea mt; mt.painingarea();");
+}
+int main()
+{int a,b,c;
+scanf("%d %d",&a,&b);
+c=a*b*27;
+printf("%d",c);
+	return 0;
+}
